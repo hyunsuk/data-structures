2f428d16e6748db04516b7576f2e1c9dc53dbd37
HyunSuk Lee <blackknight8012@gmail.com>
2017-01-06 21:40:30 +0900

    add fibonacci and termination condition comments

--- lib/recursive.h
+++ lib/recursive.h
@@ -1,8 +1,19 @@
 long factorial(int n)
 {
-    if (n <= 0) {
+    if (n <= 0) { // termination
         return 1;
     } else {
         return n * factorial(n - 1);
+    }
+}
+
+int fibonacchi(int n)
+{
+    if (n == 1) { // termination
+        return 0;
+    } else if (n == 2) { // termination
+        return 1;
+    } else {
+        return fibonacchi(n - 1) + fibonacchi(n - 2);
     }
 }
--- recursive.c
+++ recursive.c
@@ -8,4 +8,14 @@ int main(void)
     for (int i = 0; i < factorials; i++) {
         printf("%d! = %ld\n", i + 1, factorial(i + 1));
     }
+
+    const int fibonacchi_seq = 20;
+
+    for (int i = 0; i <= fibonacchi_seq; i++) {
+        if (i != fibonacchi_seq) {
+            printf("%d ", fibonacchi(i + 1));
+        } else {
+            printf("%d\n", fibonacchi(i + 1));
+        }
+    }
 }
